0b98f0c359094f1c362019fae21a6e0797d80d14
faddleb <faddlebadiane@gmail.com>
2019-10-15 13:26:05 +0100

    Housekeeping

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HealthyApp
-Testing commit
+
+Commit Info
+initialWorkBranch => Setup Login as Landing screen and added Router Dependency, need to install redux for state management and configure router properly.
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
 import { StyleSheet, Image, Text, View } from 'react-native';
-// import { connect } from 'react-redux';
-// import { LoadingView, SocialButton } from '../components';
-// import { login } from '../actions';
-import colors from '../colors';
-import fonts from '../fonts';
-// import i18n from '../i18n';
-// import imgAppIcon from '../../assets/images/app-icon.png';
-// import imgGoogleIcon from '../../assets/images/google-icon.png';
 
 export class LoginScreen extends Component {
   render() {
