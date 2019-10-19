e3b017eaf1b22fb99c9e68271f37e19ffd49295b
faddleb <faddlebadiane@gmail.com>
2019-10-19 18:30:50 +0100

    installed redux

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-/**
- * @format
- */
+// Defualt index.js
 
-import {AppRegistry} from 'react-native';
+// import {AppRegistry} from 'react-native';
+// import App from './src/App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import { AppRegistry } from 'react-native';
+import React from 'react';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './src/store';
+
+const store = configureStore()
+
+const RNRedux = () => (
+  <Provider store = { store }>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => RNRedux);
