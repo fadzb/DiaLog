4873d215148ddb63efab4d91d95b472c5f8401df
faddleb <faddlebadiane@gmail.com>
2019-11-03 21:31:49 +0000

    dependicies installed and linked

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import { RouterComponent } from './Router';
 import { connect } from 'react-redux';
 import { addName } from './actions/actions';
 import { LoginScreen } from './screens/LoginScreen';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const state = {
   name: 'user',
 };
 
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+});
+
+const AppContainer = createAppContainer(AppNavigator);
+
 const App = () => (
   <View style={{ flex: 1 }}>
-    {/* <RouterComponent></RouterComponent> */}
     <LoginScreen />
   </View>
 );
