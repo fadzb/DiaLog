a1a3288e7a712266f6ed0c0abf67158b49e07755
Faddle Badiane <faddlebadiane@gmail.com>
2019-11-06 22:26:17 +0000

    Home screen (#7)
    * initial work, added buttons for use cases
    
    * initial work, added buttons for use cases
    
    * home screen tidy up

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -3,7 +3,17 @@ import { View, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { LoginScreen } from './LoginScreen';
+import { CarbScreen } from './CarbScreen';
+import { LogActScreen } from './LogActScreen';
+import { TrainScreen } from './TrainScreen';
 import Button from '../components/Button';
+
+const labels = {
+  LOGIN: 'Login',
+  CARB: 'Estimate CHO',
+  LOG_ACT: 'Log Activity',
+  TRAIN: 'Training Modules',
+};
 
 class HomeScreen extends React.Component {
   constructor(props: Readonly<{}>) {
@@ -12,8 +22,20 @@ class HomeScreen extends React.Component {
     console.log(props);
   }
 
-  handleButtonPress = () => {
+  handleLoginNav = () => {
     this.props.navigation.navigate('Login', {});
+  };
+
+  handleEstimateNav = () => {
+    this.props.navigation.navigate('Carb', {});
+  };
+
+  handleLogActNav = () => {
+    this.props.navigation.navigate('LogAct', {});
+  };
+
+  handleTrainNav = () => {
+    this.props.navigation.navigate('Train', {});
   };
 
   render() {
@@ -26,10 +48,10 @@ class HomeScreen extends React.Component {
         }}
       >
         <Text>Home Screen</Text>
-        <Button
-          label={'Go to login page'}
-          onPress={this.handleButtonPress}
-        />
+        <Button label={labels.LOGIN} onPress={this.handleLoginNav} />
+        <Button label={labels.CARB} onPress={this.handleEstimateNav} />
+        <Button label={labels.LOG_ACT} onPress={this.handleLogActNav} />
+        <Button label={labels.TRAIN} onPress={this.handleTrainNav} />
       </View>
     );
   }
@@ -38,8 +60,10 @@ class HomeScreen extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    // Carb: CarbScreen,
     Login: LoginScreen,
+    Carb: CarbScreen,
+    LogAct: LogActScreen,
+    Train: TrainScreen,
   },
   {
     initialRouteName: 'Home',
---
+++ src/screens/TrainScreen.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export class TrainScreen extends React.Component {}
