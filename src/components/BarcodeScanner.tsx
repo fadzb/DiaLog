7a901e7d4fba488814de5469a337e63a3dc8506e
faddleb <faddlebadiane@gmail.com>
2020-01-08 19:57:49 +0000

    wip added react-native-camera

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image } from 'native-base';
+import { RNCamera } from 'react-native-camera';
+import { styles } from '../styles/CarbScreen';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Alert } from 'react-native';
 
 interface BarcodeScannerProps {
   query: string;
@@ -10,7 +14,49 @@ export class BarcodeScanner extends React.Component<BarcodeScannerProps> {
     super(props);
   }
 
+  state = {
+    torchOn: false,
+  };
+
+  onBarCodeRead = (e: any) => {
+    Alert.alert('Barcode value is' + e.data, 'Barcode type is' + e.type);
+  };
+
+  handleTourch(value: any) {
+    if (value === true) {
+      this.setState({ torchOn: false });
+    } else {
+      this.setState({ torchOn: true });
+    }
+  }
+
   render() {
-    return <View></View>;
+    return (
+      <View>
+        <Text> Barcode Scanner </Text>
+        <Camera
+          style={styles.preview}
+          torchMode={
+            this.state.torchOn ? RNCamera.Constants.FlashMode.on : RNCamera.Constants.FlashMode.off
+          }
+          onBarCodeRead={this.onBarCodeRead}
+          ref={cam => (this.camera = cam)}
+          aspect={Camera.constants.Aspect.fill}
+        >
+          <Text
+            style={{
+              backgroundColor: 'white',
+            }}
+          >
+            BARCODE SCANNER
+          </Text>
+        </Camera>
+        <View style={styles.bottomOverlay}>
+          <TouchableOpacity onPress={() => this.handleTourch(this.state.torchOn)}>
+            <Text>Torch</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
   }
 }
--- src/styles/CarbScreen.ts
+++ src/styles/CarbScreen.ts
@@ -41,4 +41,23 @@ export const styles = StyleSheet.create({
     padding: 10,
     width: '100%',
   },
+
+  //Camera styles
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  cameraIcon: {
+    margin: 5,
+    height: 40,
+    width: 40,
+  },
+  bottomOverlay: {
+    position: 'absolute',
+    width: '100%',
+    flex: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 });
