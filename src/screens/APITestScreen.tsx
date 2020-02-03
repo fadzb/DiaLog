9c53686158048a974aa3ddae8ea164047d0adad9
faddleb <faddlebadiane@gmail.com>
2020-02-03 16:42:48 +0000

    Firestore working, used invertabase tutorial, built out api test functionality

--- src/screens/APITestScreen.tsx
+++ src/screens/APITestScreen.tsx
@@ -7,6 +7,8 @@ import { Log } from '../typings/Log';
 import { aysncStoreItem, asyncGetAllKeys, asyncMultiRemove } from '../storage/AsyncStorage';
 
 import { login, signOut, getCurrentUser, register } from '../utils/FirebaseAuth/AuthUtils';
+import { ScrollView } from 'react-native';
+import { addTodo, getModules } from '../utils/FirebaseDB/FirestoreUtils';
 
 const TEST_EMAIL = 'test@gmail.com';
 const TEST_PASSWORD = '123456';
@@ -72,40 +74,62 @@ export class APITestScreen extends React.Component<APITestScreenProps> {
     signOut();
   };
 
+  addTodo = () => {
+    //Add todo to 'todos' collection
+    addTodo();
+  };
+
+  getModules = () => {
+    getModules('Diabetes Basics').then((moduleList: any) =>
+      console.log('Module list: ' + moduleList),
+    );
+  };
+
   render() {
     return (
-      <Container style={styles.container}>
-        <Header>
-          <Title>Test APIs</Title>
-        </Header>
-        <Content style={styles.contentContainer}>
-          <Title>Test AsyncStorage</Title>
-          <Button style={styles.testButtons} onPress={this.testTodaysLogs}>
-            <Text>Test Todays Logs</Text>
-          </Button>
-          <Button style={styles.testButtons} onPress={this.addNewLog}>
-            <Text>Add New Log</Text>
-          </Button>
-          <Button style={styles.testButtons} onPress={this.clearAllLogs}>
-            <Text>Clear All Logs</Text>
-          </Button>
-        </Content>
-        <Content style={styles.contentContainer}>
-          <Title>Test Firebase Auth</Title>
-          <Button style={styles.testButtons} onPress={this.register}>
-            <Text>Register</Text>
-          </Button>
-          <Button style={styles.testButtons} onPress={this.getCurrentUser}>
-            <Text>Get Current User</Text>
-          </Button>
-          <Button style={styles.testButtons} onPress={this.login}>
-            <Text>Login</Text>
-          </Button>
-          <Button style={styles.testButtons} onPress={this.signOut}>
-            <Text>Sign out</Text>
-          </Button>
-        </Content>
-      </Container>
+      <ScrollView>
+        <Container style={styles.container}>
+          <Header>
+            <Title>Test APIs</Title>
+          </Header>
+          <Content style={styles.contentContainer}>
+            <Title>Test AsyncStorage</Title>
+            <Button style={styles.testButtons} onPress={this.testTodaysLogs}>
+              <Text>Test Todays Logs</Text>
+            </Button>
+            <Button style={styles.testButtons} onPress={this.addNewLog}>
+              <Text>Add New Log</Text>
+            </Button>
+            <Button style={styles.testButtons} onPress={this.clearAllLogs}>
+              <Text>Clear All Logs</Text>
+            </Button>
+          </Content>
+          <Content style={styles.contentContainer}>
+            <Title>Test Firebase Auth</Title>
+            <Button style={styles.testButtons} onPress={this.register}>
+              <Text>Register</Text>
+            </Button>
+            <Button style={styles.testButtons} onPress={this.getCurrentUser}>
+              <Text>Get Current User</Text>
+            </Button>
+            <Button style={styles.testButtons} onPress={this.login}>
+              <Text>Login</Text>
+            </Button>
+            <Button style={styles.testButtons} onPress={this.signOut}>
+              <Text>Sign out</Text>
+            </Button>
+          </Content>
+          <Content style={styles.contentContainer}>
+            <Title>Test Firestore DB</Title>
+            <Button style={styles.testButtons} onPress={this.addTodo}>
+              <Text>Add Todo</Text>
+            </Button>
+            <Button style={styles.testButtons} onPress={this.getModules}>
+              <Text>Get Modules</Text>
+            </Button>
+          </Content>
+        </Container>
+      </ScrollView>
     );
   }
 }
