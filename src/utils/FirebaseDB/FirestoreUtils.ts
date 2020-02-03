a9a4cf95eee9b23ee050126fd1989515f45d935c
faddleb <faddlebadiane@gmail.com>
2020-02-03 15:15:17 +0000

    just commiting to push to origin

--- src/utils/FirebaseDB/FirestoreUtils.ts
+++ src/utils/FirebaseDB/FirestoreUtils.ts
@@ -1,8 +1,30 @@
-import firestore from '@react-native-firebase/firestore';
+export const test = () => {
+  const ref = firebase.firestore().collection('modules');
 
-// ref to modules collection
-const ref = firestore().collection('modules');
+  ref
+    .where('moduleGroup', '==', 'Diabetes Basics')
+    .get()
+    .then(querySnapshot => handleSnapshot(querySnapshot))
+    .catch(error => console.log('Error getting documents: ', error));
+};
 
-export function test() {
-  console.log(ref);
-}
+const handleSnapshot = (querySnapshot: any) => {
+  querySnapshot.forEach((doc: any) => {
+    console.log(doc.id, ' => ', doc.data());
+
+    //get data from doc
+    const data = doc.data();
+  });
+};
+
+// function getModules(moduleGroup) {
+//     // ref to the table
+//     const table = document.getElementById('moduleListTable');
+//     table.innerHTML = 'Loading Modules...';
+
+//     db.collection('modules')
+//       .where('moduleGroup', '==', moduleGroup)
+//       .get()
+//       .then(querySnapshot => handleSnapshot(querySnapshot))
+//       .catch(error => console.log('Error getting documents: ', error));
+//   }
