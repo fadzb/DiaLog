ce7ac1aeb40a85062235f3ae5aa9f9f279225809
faddleb <faddlebadiane@gmail.com>
2020-03-01 16:21:29 +0000

    spawn python processses

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+var spawn = require('child_process').spawn;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+
+// Not doing this atm
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+// const admin = require('firebase-admin');
+// admin.initializeApp();
+
+// TODO: Either this or use Firebase ML Kit
+// exports.processImage = functions.https.onRequest((req, res) => {
+//   var process = spawn('python', ['./processImage.py', req.query.firstname, req.query.lastname]);
+
+//   process.stdout.on('data', function(data) {
+//     res.send(data.toString());
+//   });
 // });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+//https://www.geeksforgeeks.org/run-python-script-node-js-using-child-process-spawn-method/
+exports.helloWorldFromPython = functions.https.onRequest((req, res) => {
+  var process = spawn('python', ['./hello.py', req.query.firstname, req.query.lastname]);
+
+  process.stdout.on('data', function(data) {
+    res.send(data.toString());
+  });
+});
